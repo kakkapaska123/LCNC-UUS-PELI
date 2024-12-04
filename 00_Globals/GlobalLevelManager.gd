81494b179107b1badd916de27043984d672fffee
Topisteri <topi.haavisto@gmail.com>
2024-12-04 13:32:40 +0200

    Auto Camera Limits // E08
    Kameravideo tehty loppuun ja toimii

---
+++ 00_Globals/GlobalLevelManager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+
+var current_tilemap_bounds : Array[ Vector2 ]
+signal TilemapBoundsChanged( bounds : Array[ Vector2 ] )
+
+func ChangeTilemapBounds( bounds : Array[ Vector2 ] ) -> void:
+	current_tilemap_bounds = bounds
+	TilemapBoundsChanged.emit( bounds )
+	
+	
